c710fc6b5c1d46f6ee932be544a026bc8a18282f
kcubeiitkgp <62806768+kcubeiitkgp@users.noreply.github.com>
2022-08-07 18:22:50 +0530

    Update main.js

--- main.js
+++ main.js
@@ -3,15 +3,7 @@ window.onload = function () {
   let output = document.getElementById('output')
   let timer = document.getElementById('timer')
 
-  var seconds = 00
-  var tens = 00
-  var appendTens = document.getElementById('tens')
-  var appendSeconds = document.getElementById('seconds')
-  var buttonStart = document.getElementById('button-start')
-  var buttonStop = document.getElementById('button-stop')
-  var buttonReset = document.getElementById('button-reset')
-  var Interval
-
+  
   let topics = [
     'What has been the most terrifying moment of your life so far ?',
     'What is your happiest memory ?',
